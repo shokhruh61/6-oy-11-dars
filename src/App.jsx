0bf8a18e56fc1efa58f1d92598682c0100e22a3f
shokhruh62 <mahmudovshoxruh1@gmail.com>
2024-12-08 18:55:23 +0500

    jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Register from './pages/Register'
